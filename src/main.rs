7802378c134abb9ecf369a045fed8308f4ee3571
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2024-04-02 14:16:27 +0000

    created separate input() method and added error handling

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,30 @@
-use std::io::{self, Write};
+use std::io::{self, ErrorKind, Write};
 use std::net::TcpStream;
+use std::net::ToSocketAddrs;
+use std::time::Duration;
 
 fn main() {
-    let mut host = String::new();
-    let mut port = String::new();
-    print!("host: ");
-    io::stdout().flush().expect("failed to flush stdout");
-    io::stdin()
-        .read_line(&mut host)
-        .expect("failed to read host from stdin");
-    print!("port: ");
-    io::stdout().flush().expect("failed to flush stdout");
-    io::stdin()
-        .read_line(&mut port)
-        .expect("failed to read port from stdin");
+    let host = input("host: ");
+    let port = input("port: ");
 
     if port_is_open(host.as_str(), port.as_str()) {
         println!("{}:{} is open", host, port)
+    } else {
+        println!("{}:{} is closed", host, port)
     }
+}
+
+fn input(prompt: &str) -> String {
+    let mut user_input = String::new();
+
+    print!("{}", prompt);
+    io::stdout().flush().expect("error flushing stdout");
+
+    io::stdin()
+        .read_line(&mut user_input)
+        .expect("failed to read host from stdin");
+
+    return user_input.trim_end().to_string();
 }
 
 fn port_is_open(host: &str, port: &str) -> bool {
@@ -28,7 +35,23 @@ fn port_is_open(host: &str, port: &str) -> bool {
 
     println!("scanning {}...", address);
 
-    let result = TcpStream::connect(address);
+    let mut socket_addresses = format!("{}:{}", host, port).to_socket_addrs().unwrap();
+    let socket_address = socket_addresses.next().unwrap();
+
+    println!("{}", socket_address.to_string());
+
+    let result = TcpStream::connect_timeout(&socket_address, Duration::from_secs(1));
+
+    if let Err(e) = result {
+        match e.kind() {
+            ErrorKind::TimedOut => {}
+            ErrorKind::ConnectionRefused => {}
+            _ => {
+                println!("Error: {}", e);
+            }
+        }
+        return false;
+    }
 
     return result.is_ok();
 }
